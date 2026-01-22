8dc583228beda9dcdd6d5c4cb1f9b2b19d71657b
DevCardosu <gustavocardosodot@gmail.com>
2026-01-22 18:53:16 -0300

    feat:primeiras funções de adicionar nome, preço e quantidade do produto

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import models.Product;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        Product product = new Product();
+
+        System.out.println("Enter product data ");
+        System.out.println("Name: ");
+        product.name = sc.nextLine();
+        System.out.print("Price: ");
+        product.price = sc.nextDouble();
+        System.out.println("Quantity in stock: ");
+        product.quantity = sc.nextInt();
+
+        System.out.printf(product.name + ", " + product.price + ", " + product.quantity);
+
+        sc.close();
+    }
+}
